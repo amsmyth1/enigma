35bc60a1da4460170b26e73f8342b9d76fa54943
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-14 17:49:43 -0500

    Create encryption_shifts method

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -4,6 +4,20 @@ class Encryption
   def initialize (message, key, date)
     @message = message
     @key = key
-    @date = date 
+    @date = date
   end
+
+  def encryption_shifts
+    encryption_shifts = {}
+    encryption_shifts[:A] = ((@key[0,2].to_i) + offset(-4))
+    encryption_shifts[:B] = ((@key[1,2].to_i) + offset(-3))
+    encryption_shifts[:C] = ((@key[2,2].to_i) + offset(-2))
+    encryption_shifts[:D] = ((@key[3,2].to_i) + offset(-1))
+    encryption_shifts
+  end
+
+  def offset(x)
+    (date.to_i * date.to_i).to_s[x].to_i
+  end
+
 end
