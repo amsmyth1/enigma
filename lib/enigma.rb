d84ea09c6bfd2963b03ffc1a7cdf8b38376438ec
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-14 17:20:16 -0500

    Revise method sequence in class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,24 +1,23 @@
 class Enigma
 
-  def initialize(message, key, date)
-    @shift_case = {}
-    @encription_shifts = {}
+  def initialize
   end
 
   def encrypt(message, key = key_generator, date = Today.now)
     encrypt = {}
-    encrypt[:encryption] = Encrypt.new(message, key, date)
+    encrypt[:encryption] = Encrypt.new(message, key, clean_date(date))
     encrypt[:key] = key
     encrypt[:date] = clean_date(date)
     encrypt
   end
 
   def clean_date(date)
-    date.strftime('%d%m%Y')
+    if date.class == Time
+      date.strftime('%d%m%Y')
+    end
   end
 
   def key_generator
     "%05d" %rand(99999)
   end
-
 end
