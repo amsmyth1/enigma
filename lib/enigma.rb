f94cf2a9931b9fd35ae29fa93f27c58e9ac1b4cd
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-14 17:13:17 -0500

    Create clean_date and key_generator methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,2 +1,24 @@
 class Enigma
+
+  def initialize(message, key, date)
+    @shift_case = {}
+    @encription_shifts = {}
+  end
+
+  def encrypt(message, key = key_generator, date = Today.now)
+    encrypt = {}
+    encrypt[:encryption] = Encrypt.new(message, key, date)
+    encrypt[:key] = key
+    encrypt[:date] = clean_date(date)
+    encrypt
+  end
+
+  def clean_date(date)
+    date.strftime('%d%m%Y')
+  end
+
+  def key_generator
+    "%05d" %rand(99999)
+  end
+
 end
