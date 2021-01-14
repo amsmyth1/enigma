2e7a0e4953b09291d58c13ad456af882907a329f
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-14 18:30:44 -0500

    Create shift_case_characters method

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -19,4 +19,35 @@ class Encryption
     (date.to_i * date.to_i).to_s[x].to_i
   end
 
+  # def shift_case_characters
+  #   shift_case_characters = {A: [], B: [], C: [], D: []}
+  #   shift_case_characters.each_with_index do |(alpha, character), index|
+  #     shift_case_characters[alpha] << @message[index]
+  #   end
+  # end
+
+  def shift_case_characters
+    shift_case_characters = Hash.new([])
+    @message.chars.each_with_index do |character, index|
+      shift_case_characters[alpha(character, index)] += [character]
+    end
+    shift_case_characters
+  end
+
+  def alpha(character, index)
+    shifts = [:A, :B, :C, :D]
+    if index < 4
+      alpha = shifts[index]
+    else index >= 4
+      if index % 4 == 0
+        alpha = shifts[0]
+      elsif index % 4 == 1
+        alpha = shifts[1]
+      elsif index % 4 == 2
+        alpha = shifts[2]
+      elsif index % 4 == 3
+        alpha = shifts[3]
+      end
+    end
+  end
 end
