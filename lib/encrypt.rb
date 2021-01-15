71cc83d1cb4b3bda56f71796ccc65e769962efc6
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-15 10:38:07 -0500

    Create runner file to read and write from user CLI

---
+++ lib/encrypt.rb
@@ -0,0 +1,17 @@
+require './lib/enigma'
+require './lib/encryption'
+require './lib/decryption'
+
+
+user_message = File.open(ARGV[0], "r")
+message = user_message.read.strip
+
+enigma = Enigma.new
+user_encrypt = enigma.encrypt(message)
+encrypted_user_message = user_encrypt[:encryption]
+
+encrypted_file = File.open(ARGV[1], "w")
+encrypted_file.write(encrypted_user_message)
+encrypted_file.close
+
+puts "Created '#{ARGV[1]}' with key #{user_encrypt[:key]} and date #{user_encrypt[:date]}"
