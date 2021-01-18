374e9f55ea5d58bf4a1f833fb87058329bbc3dae
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-18 16:32:51 -0500

    Clean up encryption class

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -14,31 +14,6 @@ class Encryption
                   "y", "z", " "]
   end
 
-  # def encryption_shifts
-  #   encryption_shifts = {A: 0, B: 0, C: 0, D: 0}
-  #   encryption_shifts.each_with_index do |(alpha, shift_number), index|
-  #     encryption_shifts[alpha] = ((@key[index,2].to_i) + offset(index-4))
-  #   end
-  #   encryption_shifts
-  # end
-  #
-  # def offset(x)
-  #   (date.to_i * date.to_i).to_s[x].to_i
-  # end
-
-  def shift_case_characters
-    shift_case_characters = Hash.new([])
-    @message.chars.each_with_index do |character, index|
-      shift_case_characters[alpha(character, index)] += [character]
-    end
-    shift_case_characters
-  end
-
-  def alpha(character, index)
-    shifts = [:A, :B, :C, :D]
-    alpha = shifts[(index % 4)]
-  end
-
   def encrypted_message
     encrypted_message_characters = []
     @message.downcase.chars.each_with_index do |character, index|
