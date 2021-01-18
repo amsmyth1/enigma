bc4580166714e04636fb4f273351ab2baeca9ed7
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-18 16:34:18 -0500

    Clean up unused methods in Decryption

---
+++ lib/cipherable.rb
@@ -0,0 +1,19 @@
+module Cipherable
+
+  def shifts
+    shifts = {A: 0, B: 0, C: 0, D: 0}
+    shifts.each_with_index do |(alpha, shift_number), index|
+      shifts[alpha] = ((@key[index,2].to_i) + offset(index-4))
+    end
+    shifts
+  end
+
+  def offset(x)
+    (date.to_i * date.to_i).to_s[x].to_i
+  end
+
+  def alpha(character, index)
+    shifts = [:A, :B, :C, :D]
+    alpha = shifts[(index % 4)]
+  end
+end
